3a04f17319766650d3fead260d0e72482873e4d0
wallace <wallacegibbon@aliyun.com>
2023-08-15 10:25:09 +0800

    Trivial code style changes: Spaces before and `}`/`]` and after `{`/`[` are removed to keep the space rules simple.

--- run.js
+++ run.js
@@ -9,13 +9,13 @@ import * as commander from "commander";
 var config = {};
 
 function ip_addresses() {
-	function ip_filter({ family, internal }) {
+	function ip_filter({family, internal}) {
 		return family === "IPv4" && !internal;
 	}
 	var IPs = Object.entries(os.networkInterfaces()).map(function ([_, address_list]) {
 		return address_list.filter(ip_filter);
 	});
-	return Array.prototype.concat(...IPs).map(function ({ address }) {
+	return Array.prototype.concat(...IPs).map(function ({address}) {
 		return address;
 	});
 }
@@ -28,12 +28,12 @@ function index_page(filename_lists) {
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta charset="utf-8">
 	<style>
-body { max-width: 600px; margin: 20px auto; padding: 10px; }
-ul { padding: 0; }
-li { list-style: none; word-wrap: break-word; }
-li::before { content: ">"; color: steelblue; margin-right: 2px; }
-#form { display: flex; }
-#form>input:first-child { flex: 1; }
+body {max-width: 600px; margin: 20px auto; padding: 10px;}
+ul {padding: 0;}
+li {list-style: none; word-wrap: break-word;}
+li::before {content: ">"; color: steelblue; margin-right: 2px;}
+#form {display: flex;}
+#form>input:first-child {flex: 1;}
 	</style>
 </head>
 
@@ -43,7 +43,7 @@ li::before { content: ">"; color: steelblue; margin-right: 2px; }
 		<input type="submit" value="transmit">
 	</form>
 	<ul>
-	${filename_lists.map(function (f) { return `<li>${f}</li>`; }).join("\n")}
+	${filename_lists.map(function (f) {return `<li>${f}</li>`;}).join("\n")}
 	</ul>
 </body>
 </html>
@@ -51,7 +51,7 @@ li::before { content: ">"; color: steelblue; margin-right: 2px; }
 }
 
 async function response_index(res) {
-	res.writeHead(200, { "Content-Type": "text/html" });
+	res.writeHead(200, {"Content-Type": "text/html"});
 	try {
 		var files = await fs.promises.readdir(config.path);
 		res.end(index_page(files));
@@ -61,11 +61,11 @@ async function response_index(res) {
 }
 
 function redirect_to_index(res) {
-	res.writeHead(302, { "Location": "/" });
+	res.writeHead(302, {"Location": "/"});
 	res.end();
 }
 
-function file_handler(_field_name, file, { filename }) {
+function file_handler(_field_name, file, {filename}) {
 	var out_stream = fs.createWriteStream(`${config.path}/${filename}`);
 	out_stream.on("error", console.error);
 	file.pipe(out_stream);
@@ -81,7 +81,7 @@ function base64_decode(encoded_string) {
 }
 
 function auth_fail(res) {
-	res.writeHead(401, { "www-authenticate": "Basic realm=\"enter the password\"" });
+	res.writeHead(401, {"www-authenticate": "Basic realm=\"enter the password\""});
 	res.end("");
 }
 
@@ -89,9 +89,9 @@ function get_user_from_request(req) {
 	if (!req.headers.authorization) return null;
 
 	var auth = (req.headers.authorization || "").split(" ")[1] || "";
-	var [ username, password ] = base64_decode(auth).split(":");
+	var [username, password] = base64_decode(auth).split(":");
 
-	return { username, password };
+	return {username, password};
 }
 
 function handler(req, res) {
@@ -103,9 +103,9 @@ function handler(req, res) {
 	if (req.method !== "POST") return response_index(res);
 	if (!is_multipart(req)) return redirect_to_index(res);
 
-	var bb = busboy({ headers: req.headers });
+	var bb = busboy({headers: req.headers});
 
-	bb.on("finish", function () { redirect_to_index(res); });
+	bb.on("finish", function () {redirect_to_index(res);});
 	bb.on("file", file_handler);
 	req.pipe(bb);
 }
